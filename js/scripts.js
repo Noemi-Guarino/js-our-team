75faadc0a47bb520db8b7b9d94e6f54ea6f7ea0e
Noemi-Guarino <noemi.guarino.1996@gmail.com>
2023-12-18 17:25:33 +0100

    edit

--- js/scripts.js
+++ js/scripts.js
@@ -15,54 +15,55 @@ const employers = [
     impiegato1 = {
         name: 'Wayne Barnett',
         role: 'Founder & CEO',
-        // image: 'wayne-barnett-founder-ceo.jpg'
+        image: 'wayne-barnett-founder-ceo.jpg'
     },
     impiegato2 = {
         name: 'Angela Caroll',
         role: 'Chief Editor',
-        // image: 'angela-caroll-chief-editor.jpg'
+        image: 'angela-caroll-chief-editor.jpg'
     },
     impiegato3 = {
         name: 'Walter Gordon',
         role: 'Office Manager',
-        // image: 'walter-gordon-office-manager.jpg'
+        image: 'walter-gordon-office-manager.jpg'
     },
     impiegato4 = {
         name: 'Angela Lopez',
         role: 'Social Media Manager',
-        // image: 'angela-lopez-social-media-manager.jpg'
+        image: 'angela-lopez-social-media-manager.jpg'
     },
     impiegato5 = {
         name: 'Scott Estrada',
         role: 'Developer',
-        // image: 'scott-estrada-developer.jpg'
+        image: 'scott-estrada-developer.jpg'
     },
     impiegato6 = {
         name: 'Barbara Ramos',
         role: 'Graphic Designer',
-        // image: 'barbara-ramos-graphic-designer.jpg'
+        image: 'barbara-ramos-graphic-designer.jpg'
         
     },
 ]
 
-let imgItems = [
-    'wayne-barnett-founder-ceo.jpg',
-    'angela-caroll-chief-editor.jpg',
-    'walter-gordon-office-manager.jpg',
-    'angela-lopez-social-media-manager.jpg',
-    'scott-estrada-developer.jpg',
-    'barbara-ramos-graphic-designer.jpg',
-];
+// let imgItems = [
+//     'wayne-barnett-founder-ceo.jpg',
+//     'angela-caroll-chief-editor.jpg',
+//     'walter-gordon-office-manager.jpg',
+//     'angela-lopez-social-media-manager.jpg',
+//     'scott-estrada-developer.jpg',
+//     'barbara-ramos-graphic-designer.jpg',
+// ];
 
 
 for (let i = 0; i < employers.length; i++){
     for (let key in employers[i]){
         console.log(employers[i][key]);
         teamcontainer.innerHTML +=
-        `<div>${employers[i][key]}<div><img src="${imgItems[i]}"alt=""></div></div>`;
+        `<div>${employers[i][key]}</div>`;
     }
 }
     // console.log(employers[i]);
     // 
 
 
+<div><img src="${imgItems[i]}"alt=""></div>
